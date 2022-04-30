4dfa44a015cfac16ea4e1141a87ed2c57ed87e1e
Stefan <s.vladulescu@gmail.com>
2022-04-30 22:10:50 +0300

    .

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const display = document.querySelector(".display");
 const search = document.getElementById('search-bar')
 const searchBtn = document.getElementById('search-button')
-const tempUnitBtn = document.getElementById('tempUnitBtn')
-const unitBtn = document.getElementById('unit')
-const bodyBg = document.getElementById('bodyBg')
+const forecastPanel = document.querySelector('.future');
 
 
 const country = document.getElementById('country')
 const locName = document.getElementById('locName')
 const condition = document.getElementById('condition')
 const lastUpdate = document.getElementById('last-update')
-const temp = document.getElementById('temp')
-const feelsLike = document.getElementById('feels-like')
+const tempC = document.getElementById('tempC')
+const tempF = document.getElementById('tempF')
+
+const feelsLikeC = document.getElementById('feels-likeC')
+const feelsLikeF = document.getElementById('feels-likeF')
+
 const windDir = document.getElementById('wind-dir')
 const windSpeed = document.getElementById('wind-speed')
 const precip = document.getElementById('precip')
@@ -19,10 +20,6 @@ const humid = document.getElementById('humid')
 const uv = document.getElementById('uv')
 
 let globalGeoData;
-let f_temp;
-let c_temp;
-let km_speed;
-let m_speed;
 search.addEventListener('keypress',function(event){
     if(event.key=='Enter'){
         event.preventDefault();
@@ -38,7 +35,8 @@ function setData(){
     
 }
 async function getData(loc){
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=d8d2397a14de455eaa6135143222904&q=${loc}&aqi=no`, {mode: 'cors'});
+    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=d8d2397a14de455eaa6135143222904&q=${loc}&days=5&aqi=no&alerts=no
+    `, {mode: 'cors'});
     const geoData = await response.json();
     
     console.log(geoData)
@@ -52,66 +50,94 @@ function showData(geoData){
     locName.innerHTML = geoData.location.region;
     condition.innerHTML = geoData.current.condition.text;
     lastUpdate.innerHTML = `Last updated ${geoData.current.last_updated}`
-    temp.innerHTML = `${geoData.current.temp_c} degrees C`;
-    feelsLike.innerHTML = `Feels like ${geoData.current.feelslike_c}`
+    tempC.innerHTML = `${geoData.current.temp_c} degrees C`;
+    feelsLikeC.innerHTML = `Feels like ${geoData.current.feelslike_c}`
+    tempF.innerHTML = `${geoData.current.temp_f} degrees F`;
+    feelsLikeF.innerHTML = `Feels like ${geoData.current.feelslike_f}`
     windDir.innerHTML = `Wind direction: ${geoData.current.wind_dir}`
     windSpeed.innerHTML = `Wind speed: ${geoData.current.wind_kph} k/h`
     precip.innerHTML = `Precipitations: ${geoData.current.precip_mm} mm`
     humid.innerHTML = `Humidity: ${geoData.current.humidity}`
     uv.innerHTML = `UV ${geoData.current.uv}`
-    f_temp = geoData.current.temp_f;
-    c_temp = geoData.current.temp_c;
-    km_speed = geoData.current.wind_kph;
-    m_speed = geoData.current.wind_mph;
     let status = geoData.current.condition.text.toLowerCase();
     let isDay = geoData.current.is_day;
     
     changeBg(status, isDay)
+    forecastDays(geoData);
 
 
 
 }
+function forecastDays(geoData){
+    let arrLength = geoData.forecast.forecastday.length;
+    for(let i=0;i<arrLength;i++){
+    const dayContainer = document.createElement('div')
+    dayContainer.classList.add('days')
+    forecastPanel.appendChild(dayContainer)
+
+    const currentC = document.createElement('div')
+    currentC.classList.add('currentC');
+    currentC.innerHTML = "Current " +geoData.forecast.forecastday[i].day.avgtemp_c + " C";
+    dayContainer.appendChild(currentC);
+
+    const currentF = document.createElement('div')
+    currentF.classList.add('currentF');
+    currentF.innerHTML = "Current " +geoData.forecast.forecastday[i].day.avgtemp_f + " F";
+    dayContainer.appendChild(currentF);
+
+    const minC = document.createElement('div')
+    minC.classList.add('minC');
+    minC.innerHTML ="Min " +geoData.forecast.forecastday[i].day.mintemp_c + " C";
+    dayContainer.appendChild(minC);
+
+    const minF = document.createElement('div')
+    minF.classList.add('minF');
+    minF.innerHTML = "Min "+geoData.forecast.forecastday[i].day.mintemp_f+" F";
+    dayContainer.appendChild(minF);
+
+    const maxC = document.createElement('div')
+    maxC.classList.add('maxC');
+    maxC.innerHTML = "Max "+ geoData.forecast.forecastday[i].day.maxtemp_c+ " C";
+    dayContainer.appendChild(maxC);
+
+    const maxF = document.createElement('div')
+    maxF.classList.add('maxF');
+    maxF.innerHTML ="Max "+ geoData.forecast.forecastday[i].day.maxtemp_f+" F";
+    dayContainer.appendChild(maxF);
+
+    const cond = document.createElement('div')
+    cond.classList.add('cond');
+    cond.innerHTML = geoData.forecast.forecastday[i].day.condition.text;
+    dayContainer.appendChild(cond);
 
 
+    const precip = document.createElement('div')
+    precip.classList.add('precip');
+    precip.innerHTML ="Precipitations: " +geoData.forecast.forecastday[i].day.totalprecip_mm+" mm";
+    dayContainer.appendChild(precip);
 
 
-function changeTempUnit(){
-    if(tempUnitBtn.innerHTML === 'Celsius'){
-        tempUnitBtn.innerHTML = "Farenheit";
-        temp.innerHTML = `${f_temp} degrees F`;
-    } else if(tempUnitBtn.innerHTML === 'Farenheit'){
-        tempUnitBtn.innerHTML = "Celsius";
-        temp.innerHTML = `${c_temp} degrees C`;
     }
-
 }
-function changeUnit(){
-    if(unitBtn.innerHTML == 'Miles'){
-        unitBtn.innerHTML = "KM";
-        windSpeed.innerText = `Wind speed: ${m_speed} mph`
-    } else if(unitBtn.innerHTML == 'KM'){
-        unitBtn.innerHTML = "Miles";
-        windSpeed.innerText = `Wind speed: ${km_speed} k/h`
-    }
 
-}
+
 function changeBg(status,isDay){
     if(status.includes("cloud")){
-        bodyBg.style.cssText = "background-image: url('imgs/cloudy.jpg')";
+        document.body.style.cssText = "background-image: url('imgs/cloudy.jpg')";
     }else  if(status.includes("sun")){
-        bodyBg.style.cssText = "background-image: url('imgs/sunny.jpg')";
+        document.body.style.cssText = "background-image: url('imgs/sunny.jpg')";
     }else  if(status.includes("rain")){
-        bodyBg.style.cssText = "background-image: url('imgs/rainy.jpg')";
+        document.body.style.cssText = "background-image: url('imgs/rainy.jpg')";
     }else  if(status.includes("snow")){
-        bodyBg.style.cssText = "background-image: url('imgs/snowing.jpg')";
+        document.body.style.cssText = "background-image: url('imgs/snowing.jpg')";
     }else  if(status.includes("mist")){
-        bodyBg.style.cssText = "background-image: url('imgs/mist.jpg')";
+        document.body.style.cssText = "background-image: url('imgs/mist.jpg')";
     }else  if(status.includes("clear")){
         
         if(isDay==1){
-        bodyBg.style.cssText = "background-image: url('imgs/day.jpg')";
+        document.body.style.cssText = "background-image: url('imgs/day.jpg')";
         }else if(isDay==0){
-            bodyBg.style.cssText = "background-image: url('imgs/night.jpg')";    
+            document.body.style.cssText = "background-image: url('imgs/night.jpg')";    
         }
     }
 }
